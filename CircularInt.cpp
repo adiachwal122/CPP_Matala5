e91ec3a28aaeb29fcb4ff343048f82a321c331ec
adiachwal122 <adiach122@gmail.com>
2018-04-29 20:29:52 +0300

    Update CircularInt.cpp

--- CircularInt.cpp
+++ CircularInt.cpp
@@ -110,8 +110,10 @@ CircularInt& CircularInt::operator *=(int x) {
 CircularInt& CircularInt::operator *=(CircularInt& num) {
         cout<<"only num - start = " << num.sum <<endl;
     num.sum = check(num.sum);
-    this -> sum *= num.sum;
-    this-> sum = check(this-> sum);
+            cout<<"only num - start after change = " << num.sum <<endl;
+
+    //this -> sum *= num.sum;
+    this-> sum = check(this-> sum*= num.sum);
         cout<<"only num - end = " << this-> sum <<endl;
 
     return *this;
